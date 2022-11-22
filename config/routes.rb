42cf2cbe37315e7eb64c22876037878483f66991
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-11-22 13:28:39 +0400

    Add routes for each controller. Add more routes for devise users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,27 @@
 Rails.application.routes.draw do
-  devise_for :users
   get 'home/index'
-  root 'home#index'
+  root to: 'home#index'
+
+  devise_for :users, controllers: { 
+    registrations: "users/registrations",
+  }
+
+  devise_scope :user do  
+    get 'sign_in', to: 'users/sessions#new'
+    get 'sign_up', to: 'users/registrations#new'
+    get 'sign_out', to: 'users/sessions#destroy'
+    resources :users, only: [:show]
+  end
+
+  shallow do    
+    resources :users do 
+      resources :contacts, except: [:show, :index]
+      resources :educations, except: [:show, :index]
+      resources :personal_data, except: [:show, :index]
+      resources :projects, except: [:show, :index]
+      resources :experiences, except: [:show, :index]
+      resources :skills, except: [:show, :index]
+      resources :courses, except: [:show, :index]
+    end
+  end
 end
