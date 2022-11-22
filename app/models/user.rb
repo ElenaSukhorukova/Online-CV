c66e9ca9ac778e3ed3f7319d2d948f19c5c9ccc0
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-11-22 16:43:27 +0400

    Add associations with other models to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :contacts, dependent: :destroy
+  has_many :courses, dependent: :destroy
+  has_many :educations, dependent: :destroy
+  has_many :experiences, dependent: :destroy
+  has_many :personal_details, dependent: :destroy
+  has_many :projects, dependent: :destroy
+  has_many :skills, dependent: :destroy
 end
