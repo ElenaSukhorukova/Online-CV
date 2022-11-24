682e18d4aafc11e357dcb8dc2a08eadf4b08e2a4
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-11-24 23:12:20 +0400

    Add locales config application.rb

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,9 @@ module OnlineCv
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    
+    I18n.available_locales = [:en, :ru]
+    I18n.default_locale = :en
 
     # Configuration for the application, engines, and railties goes here.
     #
