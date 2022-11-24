74cf47057613f3fcbe4e9ad2501e2b679d19840c
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-11-24 23:05:55 +0400

    Add contacts' views

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,6 +2,6 @@ class Project < ApplicationRecord
   validates :github, :description, presence: true
   validates :title, presence: true, length: { within: 3..50 }
 
-  has_many_attached :previews
+  # has_many_attached :previews
   belongs_to :user
 end
