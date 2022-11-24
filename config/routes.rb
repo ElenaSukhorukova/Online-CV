a079d9bc6347fd1fba933bd9ea6fe3c0b394ab2d
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-11-24 23:12:40 +0400

    Edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,36 @@
 Rails.application.routes.draw do
-  get 'home/index'
-  root to: 'home#index'
+  scope "(:locale)", locale: /#{I18n.available_locales.join('|')}/ do
+    devise_for :users, controllers: { registrations: "users/registrations" }
 
-  devise_for :users, controllers: { 
-    registrations: "users/registrations",
-  }
+    devise_scope :user do  
+      get 'sign_in', to: 'users/sessions#new'
+      get 'sign_up', to: 'users/registrations#new'
+      get 'sign_out', to: 'users/sessions#destroy'
+    end
 
-  devise_scope :user do  
-    get 'sign_in', to: 'users/sessions#new'
-    get 'sign_up', to: 'users/registrations#new'
-    get 'sign_out', to: 'users/sessions#destroy'
-    resources :users, only: [:show]
-  end
+    get 'home', to: 'home#index'
+    get 'admin', to: 'home#admin'
+    get 'user', to: 'users#show'
+    root to: 'home#index'
 
-  shallow do    
-    resources :users do 
-      resources :contacts, except: [:show, :index]
-      resources :educations, except: [:show, :index]
-      resources :personal_data, except: [:show, :index]
-      resources :projects, except: [:show, :index]
-      resources :experiences, except: [:show, :index]
-      resources :skills, except: [:show, :index]
-      resources :courses, except: [:show, :index]
+    resources :contacts, only: [:index]
+    resources :educations, only: [:index]
+    resources :personal_details, only: [:index]
+    resources :projects, only: [:index]
+    resources :experiences, only: [:index]
+    resources :skills, only: [:index]
+    resources :courses, only: [:index]
+
+    shallow do    
+      resources :users do 
+        resources :contacts, except: [:index]
+        resources :educations, except: [:index]
+        resources :personal_details, except: [:index]
+        resources :projects, except: [:index]
+        resources :experiences, except: [:index]
+        resources :skills, except: [:index]
+        resources :courses, except: [:index]
+      end
     end
   end
 end
