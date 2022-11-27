75ce16a08b7ccacd723921e8af25b1f7828d2364
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-11-27 13:38:40 +0400

    Update validations

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,7 +1,11 @@
 class Contact < ApplicationRecord
-  validates :address, :linkedin, :github, :phone, presence: true
+  validates :address, :phone, presence: true
   validates :email, presence: true, format: { with: /([\w\-\.])+@([\w\-\.]+\.)+([a-z]{2,4})/ }
   validates :telegram, presence: true, format: { with: /\A@.*/, message: "Telegram should begin from @" }
-  
+  validates :linkedin, :github, presence: true, 
+    format: { 
+      with: /(((http(s?):\/\/)+(www\.)?)|(www\.))?+[a-zA-Z0-9\.\-\_]+(\.[a-zA-Z]{2,3})+(\/[a-zA-Z0-9\_\-\s\.\/\?\%\#\&\=]*)?/, 
+      message: "should begin from one of www, http://, https:// and has end domain.com/org/ru(or another)" }
+
   belongs_to :user
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,10 @@
 class Project < ApplicationRecord
-  validates :github, :description, presence: true
+  validates :description, presence: true
   validates :title, presence: true, length: { within: 3..50 }
+  validates :github, presence: true, 
+    format: { 
+      with: /(((http(s?):\/\/)+(www\.)?)|(www\.))?+[a-zA-Z0-9\.\-\_]+(\.[a-zA-Z]{2,3})+(\/[a-zA-Z0-9\_\-\s\.\/\?\%\#\&\=]*)?/, 
+      message: "should begin from one of www, http://, https:// and has end domain.com/org/ru(or another)" }
 
   # has_many_attached :previews
   belongs_to :user
