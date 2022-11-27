17496d47679e00835ce536bfeb3c0e63b4454f5e
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-11-27 13:38:59 +0400

    Change routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
     devise_for :users, controllers: { registrations: "users/registrations" }
 
     devise_scope :user do  
-      get 'sign_in', to: 'users/sessions#new'
+      get 'sign_in', to: 'devise/sessions#new'
       get 'sign_up', to: 'users/registrations#new'
-      get 'sign_out', to: 'users/sessions#destroy'
+      get 'sign_out', to: 'devise/sessions#destroy'
     end
 
     get 'home', to: 'home#index'
@@ -13,23 +13,15 @@ Rails.application.routes.draw do
     get 'user', to: 'users#show'
     root to: 'home#index'
 
-    resources :contacts, only: [:index]
-    resources :educations, only: [:index]
-    resources :personal_details, only: [:index]
-    resources :projects, only: [:index]
-    resources :experiences, only: [:index]
-    resources :skills, only: [:index]
-    resources :courses, only: [:index]
-
     shallow do    
       resources :users do 
-        resources :contacts, except: [:index]
-        resources :educations, except: [:index]
-        resources :personal_details, except: [:index]
-        resources :projects, except: [:index]
-        resources :experiences, except: [:index]
-        resources :skills, except: [:index]
-        resources :courses, except: [:index]
+        resources :contacts, except: %i[index show]
+        resources :educations, except: %i[index show]
+        resources :personal_details, except: %i[index show]
+        resources :projects, except: %i[index show]
+        resources :experiences, except: %i[index show]
+        resources :skills, except: %i[index show]
+        resources :courses, except: %i[index show]
       end
     end
   end
