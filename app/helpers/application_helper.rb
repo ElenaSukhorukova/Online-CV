3ab0b93f014efb25f20e18dae5aea03fc9656268
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-12-04 10:28:17 +0400

    Edit and add new helpers for titles, menu and names

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,27 @@
 module ApplicationHelper
+  def i18n_model_name(model, count: 1)
+    model.model_name.human(count: count)
+  end
+
+  def attribute_model(model, attr)
+    model.class.human_attribute_name(attr)
+  end
+
+  def full_title(page_title = '')
+    base_title = t('page.site_name')
+    page_title ? "#{page_title} | #{base_title}" : "#{base_title}"
+  end
+
+  def currently_at(current_page = '')
+    render partial: 'shared/menu', locals: { current_page: current_page }
+  end
+
+  def nav_tab(title, url, options = {})
+    current_page = options.delete :current_page
+    css_text = current_page == title ? 'text-success' : 'text-black'
+    options[:class] = options[:class] ? (options[:class] + ' ' + css_text) : 
+                                        css_text
+    
+    link_to title, url, options
+  end
 end
--- app/helpers/attribute_model_helper.rb
+++
@@ -1,5 +0,0 @@
-module AttributeModelHelper
-  def attribute_model(model, attr)
-    model.class.human_attribute_name(attr)
-  end
-end
