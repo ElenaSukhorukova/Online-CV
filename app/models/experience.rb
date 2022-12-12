ab88860dd8fe54595b07d25d60a17429c15f647d
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-12-12 10:04:22 +0400

    Add validation vor experence's model

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -11,6 +11,13 @@ class Experience < ApplicationRecord
   validates :date_of_end, allow_blank: true, format: { with: /\d{4}\-\d{2}\-\d{2}/,
       message: "A format of date is not right" 
     }
+  validate :check_date
+
+  def check_date
+    if date_of_begin.year > date_of_end.year
+      errors.add(:date_of_end, 'a year of date of the end should be less than year of date of the beging')
+    end
+  end
 
   belongs_to :user
 end
