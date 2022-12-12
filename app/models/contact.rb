e0960498c9a7170f965524828fd2f8dffab73f5c
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-12-12 10:04:00 +0400

    Add validation for contact

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,7 +1,12 @@
 class Contact < ApplicationRecord
-  validates :address, :phone, presence: true
+  validates :address, presence: true
   validates :email, presence: true, format: { with: /([\w\-\.])+@([\w\-\.]+\.)+([a-z]{2,4})/ }
   validates :telegram, presence: true, format: { with: /\A@.*/, message: "Telegram should begin from @" }
+  validates :phone, presence: true, 
+    format: { 
+      with: /(\d|\+\d)+\d{10}/,
+      message: 'A phone number should be 11 numbers'
+    }
   validates :linkedin, :github, presence: true, 
     format: { 
       with: /(((http(s?):\/\/)+(www\.)?)|(www\.))?+[a-zA-Z0-9\.\-\_]+(\.[a-zA-Z]{2,3})+(\/[a-zA-Z0-9\_\-\s\.\/\?\%\#\&\=]*)?/, 
