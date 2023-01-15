3802b006673c6a9d3d097312f18cb3797a29c5ec
Elena Sukhorukova <suhorukova.el@gmail.com>
2023-01-15 20:01:36 +0400

    Add skill's factory

---
+++ spec/factories/skill.rb
@@ -0,0 +1,15 @@
+FactoryBot.define do
+  factory :skill_en, class: Skill do
+    user { User.take || create(:user) }
+    skillname { Faker::Job.key_skill }
+    description { Faker::Lorem.paragraph(sentence_count: 3, supplemental: true, random_sentences_to_add: 4) }
+    locale { 'en' }
+  end
+
+  factory :skill_ru, class: Skill do
+    user { User.take || create(:user) }
+    skillname { "Навык" }
+    description { 'Не длинное описание навыка. Где получен. Какой уровень' }
+    locale { 'ru' }
+  end
+end
