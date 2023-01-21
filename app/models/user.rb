73bc6497aea2c596d3f15ae0d4cbcb7ffa38e13b
Elena Sukhorukova <suhorukova.el@gmail.com>
2023-01-21 19:46:37 +0400

    Add enum to  users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  enum role: { admin: 0 }, _suffix: :role
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
