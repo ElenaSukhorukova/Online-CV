9823d27f0c08088461f1109cad47b6952de1ca56
Elena Sukhorukova <suhorukova.el@gmail.com>
2023-01-21 19:45:49 +0400

    Add athorixation's concern for Pundit

---
+++ app/controllers/concerns/authorization.rb
@@ -0,0 +1,16 @@
+module Authorization
+  extend ActiveSupport::Concern
+
+  included do
+    include Pundit::Authorization
+
+    rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized 
+    
+    private
+
+    def user_not_authorized
+      flash[:danger] = t('flash.not_authorized')
+      redirect_to request.referer || root_path
+    end
+  end
+end
